90610ecd25d602b357f85916e996193753629634
I0I-I0I <azudjndj@gmail.com>
2026-01-23 23:07:32 +0300

    refactor: config

--- init.el
+++ init.el
@@ -6,11 +6,18 @@
 ;; make -j$(nproc)
 ;; sudo make install
 
-(setq custom-file "~/custom.el")
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 (when (eq system-type 'windows-nt)
   (setenv "HOME" (getenv "USERPROFILE"))
-  (setq default-directory (concat (getenv "HOME") "/")))
+  (setq default-directory (file-name-as-directory (getenv "HOME"))))
+
+(when (display-graphic-p)
+  (let ((font (if (eq system-type 'gnu/linux)
+                  "FantasqueSansM Nerd Font 20"
+                "FantasqueSansM Nerd Font 16")))
+    (when (find-font (font-spec :name font))
+      (set-frame-font font nil t))))
 
 (declare-function cape-keyword "cape")
 (declare-function ffap-file-at-point "ffap")
@@ -19,114 +26,110 @@
 
 (defvar my/gc-cons-threshold-orig gc-cons-threshold)
 (defvar my/gc-cons-percentage-orig gc-cons-percentage)
-(setq gc-cons-threshold (* 256 1024 1024))
-(setq read-process-output-max (* 4 1024 1024))
-(setq comp-async-jobs-number 8)
-(setq comp-deferred-compilation t)
-(setq gc-cons-percentage 0.6)
 
-(defvar my/file-name-handler-alist-orig file-name-handler-alist)
-(setq file-name-handler-alist nil)
+;; Core
+(use-package emacs
+  :ensure nil
+  :init
+  (setq gc-cons-threshold (* 256 1024 1024)
+        gc-cons-percentage 0.6
+        read-process-output-max (* 4 1024 1024)
+        comp-async-jobs-number 8
+        comp-deferred-compilation t)
 
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (setq gc-cons-threshold my/gc-cons-threshold-orig
-                  gc-cons-percentage my/gc-cons-percentage-orig
-                  file-name-handler-alist my/file-name-handler-alist-orig)
-            (run-with-idle-timer 5 nil #'garbage-collect)))
+  (add-hook 'emacs-startup-hook
+            (lambda ()
+              (setq gc-cons-threshold my/gc-cons-threshold-orig
+                    gc-cons-percentage my/gc-cons-percentage-orig)
+              (run-with-idle-timer 5 nil #'garbage-collect)))
 
-(setq auto-save-default nil)
-(setq make-backup-files nil)
+  (setq auto-save-default nil
+        make-backup-files nil)
 
-(when (boundp 'native-comp-async-report-warnings-errors)
-  (setq native-comp-async-report-warnings-errors nil))
-(when (boundp 'native-comp-deferred-compilation)
-  (setq native-comp-deferred-compilation t))
+  (when (boundp 'native-comp-async-report-warnings-errors)
+    (setq native-comp-async-report-warnings-errors nil))
+  (when (boundp 'native-comp-deferred-compilation)
+    (setq native-comp-deferred-compilation t))
 
-(setq use-package-verbose nil)
+  (run-with-idle-timer 10 t #'garbage-collect)
 
-(run-with-idle-timer 10 t #'garbage-collect)
+  (set-face-attribute 'tab-bar nil :height 160)
+  (setq tab-bar-close-button-show nil
+        tab-bar-new-button-show nil)
 
-(set-face-attribute 'tab-bar nil :height 160)
-(setq tab-bar-close-button-show nil
-      tab-bar-new-button-show nil)
+  (setq display-line-numbers-type 'relative)
+  (global-display-line-numbers-mode 1)
 
-;; Maple Mono 17
-(set-frame-font (if (eq system-type 'gnu/linux)
-                    "FantasqueSansM Nerd Font 20"
-                  "FantasqueSansM Nerd Font 16")
-                nil t)
+  (setq-default fill-column 80
+                indent-tabs-mode nil
+                tab-width 4
+                c-basic-offset 4
+                standard-indent 4)
+  (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
 
-(require 'grep)
-(setq grep-command "grep -nH --color=auto -r -e ")
+  (fringe-mode '(8 . 8))
+  (electric-indent-mode 1)
+  (setq inhibit-startup-screen t
+        ring-bell-function 'ignore
+        display-time-default-load-average nil)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (add-hook 'before-save-hook #'delete-trailing-whitespace)
+  (save-place-mode 1)
+  (global-hl-line-mode 1)
 
-(setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode 1)
+  (global-auto-revert-mode 1)
+  (setq auto-revert-verbose nil)
+  (fset 'yes-or-no-p 'y-or-n-p)
+  (setq savehist-additional-variables '(register-alist))
+  (savehist-mode 1)
 
-(setq-default fill-column 80)
-(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
+  (custom-set-variables
+   '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))))
 
-(fringe-mode '(8 . 8))
+;; Grep
+(use-package grep
+  :ensure nil
+  :custom
+  (grep-command "grep -nH --color=auto -r -e "))
 
-(setq-default indent-tabs-mode nil)     ;; expandtab
-(setq-default tab-width 4)
-(setq-default c-basic-offset 4)
-(setq-default standard-indent 4)
-(electric-indent-mode 1)                ;; smart indent-ish
-(setq inhibit-startup-screen t)
-(setq ring-bell-function 'ignore)
-(setq display-time-default-load-average nil)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(add-hook 'before-save-hook #'delete-trailing-whitespace)
-(save-place-mode 1)
-(global-hl-line-mode 1)
+;; Whitespace
+(use-package whitespace
+  :ensure nil
+  :custom
+  (whitespace-style '(face empty tabs lines-tail trailing))
+  (whitespace-line-column 120)
+  :config
+  (global-whitespace-mode t))
 
-(global-auto-revert-mode 1)
-(setq auto-revert-verbose nil)
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq savehist-additional-variables '(register-alist))
-(savehist-mode 1)
-(which-key-mode 1)
-(editorconfig-mode 1)
+;; Completion UI
+(use-package icomplete
+  :ensure nil
+  :init
+  (fido-vertical-mode 1)
+  :custom
+  (completion-styles '(basic flex))
+  (completions-format 'one-column)
+  (completions-max-height 15)
+  (completions-sort 'historical)
+  (completion-auto-select nil)
+  (completions-detailed t)
+  (completions-highlight-face 'completions-highlight)
+  (completion-auto-help 'visible))
 
-(prefer-coding-system 'utf-8-unix)
-(setq-default buffer-file-coding-system 'utf-8-unix)
-
-(custom-set-variables
- '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
-
-(require 'whitespace)
-(setq whitespace-style '(face empty tabs lines-tail trailing))
-(setq whitespace-line-column 80)
-(global-whitespace-mode t)
-
-;; (global-completion-preview-mode 1)
-(fido-vertical-mode 1)
-(setq completion-styles '(basic flex)
-      completions-format 'vertical
-      completions-max-height 15
-      completions-sort 'historical
-      completion-auto-select nil)
-
-(setq completions-detailed t
-      completions-highlight-face 'completions-highlight)
-
-(setq completion-auto-help 'visible
-      completions-format 'one-column)
-
+;; Navigation
 (defun scroll-half-page-down ()
   "scroll down half the page"
   (interactive)
   (scroll-down (/ (window-body-height) 2)))
+
 (defun scroll-half-page-up ()
   "scroll up half the page"
   (interactive)
   (scroll-up (/ (window-body-height) 2)))
 
-;; Keymaps
-
+;; Editing helpers
 (defun move-text-internal (arg)
   (cond
    ((and mark-active transient-mark-mode)
@@ -160,6 +163,7 @@
   (interactive "*p")
   (move-text-internal (- arg)))
 
+;; Files
 (defun open-file-at-point ()
   "Open the file name at point. Supports ~/ and ./ paths."
   (interactive)
@@ -169,6 +173,7 @@
         (find-file (expand-file-name file))
       (user-error "No filename at point"))))
 
+;; Keymaps
 (global-unset-key (kbd "C-z"))
 (defvar my/c-z-map (let ((m (make-sparse-keymap))) m))
 (keymap-global-set "C-z" my/c-z-map)
@@ -185,23 +190,36 @@
 (keymap-global-set "M-p" #'move-text-up)
 (keymap-global-set "M-n" #'move-text-down)
 
-;; Plugins
+;; Packages
+(use-package package
+  :ensure nil
+  :init
+  (setq package-archives
+        '(("gnu"   . "https://elpa.gnu.org/packages/")
+          ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+          ("melpa" . "https://melpa.org/packages/")))
+  (package-initialize)
+  (unless package-archive-contents
+    (package-refresh-contents)))
 
-(require 'package)
-(setq package-archives
-      '(("gnu"   . "https://elpa.gnu.org/packages/")
-        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-        ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
-(setq use-package-always-ensure t)
+(use-package use-package
+  :ensure t
+  :config
+  (setq use-package-verbose nil)
+  :custom
+  (use-package-always-ensure t))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode 1))
+
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
 
 ;; Theme
-
 (use-package almost-mono-themes
   :ensure t
   :config
@@ -216,13 +234,13 @@
      '(tab-bar-tab ((t (:background "#222222" :foreground "#c5c9c5" :weight bold))))
      '(tab-bar-tab-inactive ((t (:background nil :foreground "#565f89" :box nil)))))
     (setq frame-background-mode 'dark)
-    (set-frame-parameter nil 'alpha-background 70)
-    (add-to-list 'default-frame-alist '(alpha-background . 70))
-    (add-to-list 'default-frame-alist '(background-color . "#000000"))
-    (set-face-background 'default "#000000")))
+    (when (display-graphic-p)
+      (set-frame-parameter nil 'alpha-background 70)
+      (add-to-list 'default-frame-alist '(alpha-background . 70))
+      (add-to-list 'default-frame-alist '(background-color . "#000000"))
+      (set-face-background 'default "#000000"))))
 
 ;; IBuffer
-
 (use-package ibuffer
   :ensure nil
   :hook (ibuffer-mode . (lambda ()
@@ -252,13 +270,12 @@
                         (name . "^\\*Completions\\*$")))
            ("Dired" (mode . dired-mode))
            ("Magit" (name . "^\\*magit"))
-           ("Process" (name . "^\\*"))
-           )))
+           ("Process" (name . "^\\*")))))
 
-  (setq ibuffer-show-empty-filter-groups nil)
-  (setq ibuffer-always-show-last-buffer nil)
-  (setq ibuffer-default-sorting-mode 'recency)
-  (setq ibuffer-human-readable-size t)
+  (setq ibuffer-show-empty-filter-groups nil
+        ibuffer-always-show-last-buffer nil
+        ibuffer-default-sorting-mode 'recency
+        ibuffer-human-readable-size t)
 
   :bind (("C-x C-b" . ibuffer)
          :map ibuffer-mode-map
@@ -268,6 +285,7 @@
          ("C-c / f" . ibuffer-filter-by-filename)
          ("C-c / c" . ibuffer-clear-filter-groups)))
 
+;; Buffer helpers
 (defun buffers-with-mode (mode)
   (let* ((name (if (symbolp mode) (symbol-name mode) mode))
          (mode-sym (intern (if (string-suffix-p "-mode" name)
@@ -330,37 +348,90 @@
 
 (keymap-set my/c-z-map "C-z b" (lambda () (interactive) (switch-to-buffer-with-mode 'eaf-mode)))
 (keymap-set my/c-z-map "b" (lambda () (interactive) (switch-to-buffer-with-mode 'prog-mode)))
-;; (keymap-global-set "C-x b" (lambda () (interactive) (my/switch-to-buffer-excluding-modes '(eaf-mode prog-mode))))
+(keymap-set my/c-z-map "e" (lambda () (interactive) (switch-to-buffer-with-mode 'eshell-mode)))
 
 ;; Eshell
-
 (use-package eshell
   :ensure nil
   :hook (eshell-mode . (lambda () (display-line-numbers-mode -1)))
+  :custom
+  (eshell-scroll-to-bottom-on-input 'all)
+  :bind (("C-c e" . eshell))
   :config
-  (setq eshell-scroll-to-bottom-on-input 'all)
-  :bind (("C-c e" . eshell)))
+  (setq eshell-cmpl-cycle-completions nil
+        eshell-cmpl-ignore-case t
+        eshell-cmpl-autolist t
+        eshell-cmpl-expand-tildes t
+        eshell-cmpl-replace-by-expanded-string t)
+  (setq eshell-history-size 5000
+        eshell-scroll-to-bottom-on-input t
+        eshell-scroll-show-maximum-output t
+        eshell-hist-ignoredups t
+        eshell-prefer-lisp-functions nil)
+
+  (defun my/eshell-history-fuzzy ()
+    (interactive)
+    (require 'em-hist)
+    (let* ((items (and eshell-history-ring
+                       (delete-dups (ring-elements eshell-history-ring))))
+           (choice (completing-read "Eshell history: " items nil t)))
+      (when (and choice (not (string-empty-p choice)))
+        (let ((bol (save-excursion
+                     (eshell-bol)
+                     (point))))
+          (delete-region bol (point))
+          (insert choice)))))
+
+  (add-hook 'eshell-mode-hook
+            (lambda ()
+              (keymap-set eshell-mode-map "C-r" #'my/eshell-history-fuzzy)
+              (keymap-set eshell-mode-map "C-l" #'eshell/clear))))
+
+(use-package bash-completion
+  :ensure t
+  :after eshell
+  :if (or (and (eq system-type 'gnu/linux)
+               (executable-find "bash"))
+          (and (eq system-type 'windows-nt)
+               (executable-find "wsl.exe")))
+  :config
+  (when (eq system-type 'windows-nt)
+    (setq bash-completion-prog "wsl.exe")
+    (setq bash-completion-args '("--" "bash")))
+
+  (defun +eshell-bash-completion-capf-nonexclusive ()
+    (let* ((bol-pos (save-mark-and-excursion
+                      (eshell-bol)
+                      (point)))
+           (compl (bash-completion-dynamic-complete-nocomint
+                   bol-pos
+                   (point) t)))
+      (when compl
+        (append compl '(:exclusive no)))))
+
+  (defun +eshell-setup-bash-completion-h ()
+    (add-hook 'completion-at-point-functions
+              #'+eshell-bash-completion-capf-nonexclusive nil t))
+  (add-hook 'eshell-mode-hook #'+eshell-setup-bash-completion-h))
 
 (use-package eat
   :ensure t
   :commands (eat eat-eshell-mode)
   :hook (eshell-mode . eat-eshell-mode))
 
-;; Undo tree
-
+;; Undo
 (use-package undo-tree
   :ensure t
-  :init
-  (setq undo-tree-visualizer-timestamps t
-        undo-tree-history-directory-alist `(("." . ,(expand-file-name "undo-history" user-emacs-directory)))
-        undo-tree-auto-save-history t
-        undo-limit 10000000
-        undo-strong-limit 10000000)
+  :custom
+  (undo-tree-visualizer-timestamps t)
+  (undo-tree-history-directory-alist `(("." . ,(expand-file-name "undo-history" user-emacs-directory))))
+  (undo-tree-auto-save-history t)
+  (undo-limit 10000000)
+  (undo-strong-limit 10000000)
   :config
   (global-undo-tree-mode))
 
-;;; Git
-
+;; Git
 (use-package magit
   :ensure t
   :commands (magit-status magit-dispatch)
@@ -372,8 +443,7 @@
             :branch "master")
   :bind (("C-z g t" . git-timemachine)))
 
-;; Expand region
-
+;; Selection
 (use-package expand-region
   :ensure t
   :bind (("M-h" . er/expand-region)))
@@ -390,8 +460,7 @@
          ("C-M->" . mc/skip-to-next-like-this)
          ("C-M-<" . mc/skip-to-previous-like-this)))
 
-;; Jumps
-
+;; Jump
 (use-package avy
   :vc (:url "https://github.com/abo-abo/avy"
             :rev :newest
@@ -399,7 +468,6 @@
   :bind (("C-'" . avy-goto-word-1)))
 
 ;; LSP
-
 (use-package eglot
   :ensure t
   :commands (eglot eglot-ensure)
@@ -416,14 +484,18 @@
    (c-mode . eglot-ensure)
    (cpp-mode . eglot-ensure))
   :config
-  ;; pip install rassumfrassum (https://github.com/joaotavora/rassumfrassum)
+  ;; https://getzana.net/#download
 
-  ;; uv tool install ty ruff
+  ;; zana install \
+  ;; npm:vscode-langservers-extracted \
+  ;; npm:typescript-language-server \
+  ;; npm:typescript
+
+  ;; uv tool install ruff ty
+
   (add-to-list 'eglot-server-programs
                '((python-ts-mode python-mode)
                  . ("rass" "--" "ty" "server" "--" "ruff" "server")))
-
-  ;; bun install -g vscode-langservers-extracted @biomejs/biome typescript-language-server typescript
   (add-to-list 'eglot-server-programs
                '((html-mode)
                  . ("rass" "--" "vscode-html-language-server" "--stdio" "--" "biome" "lsp-proxy")))
@@ -433,8 +505,6 @@
   (add-to-list 'eglot-server-programs
                '((typescript-ts-mode typescript-mode js-mode js2-mode)
                  . ("rass" "--" "typescript-language-server" "--stdio" "--" "biome" "lsp-proxy")))
-
-  ;; sudo dnf install clang-tools-extra (Fedora)
   (add-to-list 'eglot-server-programs
                '((c-mode c-ts-mode c++-mode c++-ts-mode)
                  . ("clangd"
@@ -461,18 +531,18 @@
   :ensure t
   :init
   (global-corfu-mode 1)
-  (setq corfu-auto t
-        corfu-cycle t
-        corfu-echo-delay 0.1
-        corfu-popupinfo-delay 0.1))
+  :custom
+  (setq corfu-auto-trigger ".")
+  (corfu-auto t)
+  (corfu-cycle t)
+  (corfu-echo-delay 0.2)
+  (corfu-popupinfo-delay 0.2))
 
 (use-package cape
   :ensure t
   :init
   (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-  (add-hook 'completion-at-point-functions #'cape-dabbrev)
-  (add-hook 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-keyword)
   (add-hook 'completion-at-point-functions #'cape-elisp-block))
 
@@ -494,8 +564,6 @@
          ("\\.tsx\\'" . tsx-ts-mode)))
 
 ;; AI
-
-;; M-x copilot-install-server
 (use-package copilot
   :vc (:url "https://github.com/copilot-emacs/copilot.el"
             :rev :newest
@@ -516,7 +584,6 @@
   :bind (("C-z a c" . agent-shell)))
 
 ;; Zen mode
-
 (use-package sublimity
   :ensure t
   :config
@@ -524,27 +591,19 @@
   (require 'sublimity-attractive)
   :bind (("C-z z" . sublimity-mode)))
 
-;; Utils
-
-;;; Video player
-
+;; Media
 (use-package emms
   :ensure t)
 
-;; https://github.com/lorniu/mpvi
-;; ~/.config/mpv/input.conf
-;; %APPDATA%\mpv\input.conf
-;;;  Ctrl+r cycle-values video-rotate "0" "270" "180" "90"
+;; Video
 (use-package mpvi
   :ensure t
-  :config
-  (setq mpvi-mpv-ontop-p t)
+  :custom
+  (mpvi-mpv-ontop-p t)
   :bind (("C-z C-v" . mpvi-play)
          ("C-z v s" . mpvi-speed)))
 
-;; Other
-
-;; Do not forget to run 'M-x eaf-install' after installation
+;; EAF
 (use-package eaf
   :vc (:url "https://github.com/emacs-eaf/emacs-application-framework"
             :rev :newest
